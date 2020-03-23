450a7b65d99525ace5308c36f8914a5da25d67df
Xavier Connelly <44646743+xavierconnelly@users.noreply.github.com>
2020-03-24 10:33:53 +1100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
+function openFaqs() {
+  document.getElementById("faqs").style.width = "100%";
+}
+function closeFaqs() {
+  document.getElementById("faqs").style.width = "0";
+}
 
+function openAbout() {
+  document.getElementById("about").style.width = "100%";
+}
+function closeAbout() {
+  document.getElementById("about").style.width = "0";
+}
+
+function openDisclaimer() {
+  document.getElementById("disclaimer").style.width = "100%";
+}
+function closeDisclaimer() {
+  document.getElementById("disclaimer").style.width = "0";
+}
