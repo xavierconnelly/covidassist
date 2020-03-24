e9864eeb5c7c3d720d30234e103cdbd44cc42325
Xavier Connelly <44646743+xavierconnelly@users.noreply.github.com>
2020-03-24 22:38:03 +1100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,39 +3,37 @@ function openJosef() {
     document.getElementById("josef").style.display = "block";
 }
 
-
 function openMenu(x) {
-    x.classList.toggle("change");
-    
-    var z = window.matchMedia("(max-width: 900px)")
-    if (z.matches) { // If less than 900px
-    
-    var y = document.getElementById("menu");
-    if (y.style.left === "0vw") {
+  x.classList.toggle("change");
+  
+  var z = window.matchMedia("(max-width: 900px)")
+  
+  if (z.matches) { // If less than 800px
+   var y = document.getElementById("menu");
+      if (y.style.left === "0vw") {
         y.style.left = "100vw";
-        document.getElementById("faqs").style.left = "0vw";
-        document.getElementById("about").style.left = "0vw";
-        document.getElementById("disclaimer").style.left = "0vw";
-  } else {
+        document.getElementById("faqs").style.left = "100vw";
+        document.getElementById("about").style.left = "100vw";
+        document.getElementById("disclaimer").style.left = "100vw";
+      } else {
         y.style.left = "0vw";
-    }
-  };  else {
-        var y = document.getElementById("menu");
+      }
+  } else {
+      var y = document.getElementById("menu");
       if (y.style.left === "50vw") {
-          y.style.left = "100vw";
-          document.getElementById("faqs").style.left = "100vw";
-          document.getElementById("about").style.left = "100vw";
-          document.getElementById("disclaimer").style.left = "100vw";
+        y.style.left = "100vw";
+        document.getElementById("faqs").style.left = "100vw";
+        document.getElementById("about").style.left = "100vw";
+        document.getElementById("disclaimer").style.left = "100vw";
       } else {
         y.style.left = "50vw";
       }
-  };
+  }; 
 }
 
-
 function openFaqs() {
-  var b = window.matchMedia("(max-width: 900px)")
-  if (b.matches) { // If less than 900px
+  var b = window.matchMedia("(max-width: 800px)")
+  if (b.matches) { // If less than 800px
       document.getElementById("faqs").style.left = "0vw";
       document.getElementById("about").style.left = "100vw";
       document.getElementById("disclaimer").style.left = "100vw";
@@ -73,6 +71,4 @@ function openDisclaimer() {
       document.getElementById("about").style.left = "50vw";
       document.getElementById("disclaimer").style.left = "0vw";
   }
-} 
-
-
+}
