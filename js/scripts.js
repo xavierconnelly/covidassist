bfdadefe31ab8530d1c0ec57cc830c143a9b5fda
Xavier Connelly <44646743+xavierconnelly@users.noreply.github.com>
2020-03-25 21:36:20 +1100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,78 @@
+function openJosef() {
+    document.getElementById("view").style.display = "none";
+    document.getElementById("josef").style.display = "block";
+}
+
+function openMenu(x) {
+    x.classList.toggle("change");
+    
+    var z = window.matchMedia("(max-width: 900px)")
+    if (z.matches) { // If less than 900px
+  
+    var y = document.getElementById("menu");
+    if (y.style.left === "0vw") {
+        y.style.left = "100vw";
+        document.getElementById("faqs").style.left = "0vw";
+        document.getElementById("about").style.left = "0vw";
+        document.getElementById("disclaimer").style.left = "0vw";
+  } else {
+        y.style.left = "0vw";
+    }
+  } else {
+      var y = document.getElementById("menu");
+      if (y.style.left === "50vw") {
+          y.style.left = "100vw";
+          document.getElementById("faqs").style.left = "100vw";
+          document.getElementById("about").style.left = "100vw";
+          document.getElementById("disclaimer").style.left = "100vw";
+      } else {
+        y.style.left = "50vw";
+      }
+  };
+}
+
+function openFaqs() {
+  var b = window.matchMedia("(max-width: 900px)")
+  if (b.matches) { // If less than 900px
+      document.getElementById("faqs").style.left = "0vw";
+      document.getElementById("about").style.left = "100vw";
+      document.getElementById("disclaimer").style.left = "100vw";
+      document.getElementById("menu").style.left = "100vw";
+  } else {
+      document.getElementById("faqs").style.left = "0vw";
+      document.getElementById("about").style.left = "50vw";
+      document.getElementById("disclaimer").style.left = "50vw";
+  }
+}
+
+function openAbout() {
+  var b = window.matchMedia("(max-width: 900px)")  
+  if (b.matches) { // If less than 900px
+      document.getElementById("faqs").style.left = "0vw";
+      document.getElementById("about").style.left = "0vw";
+      document.getElementById("disclaimer").style.left = "100vw";
+      document.getElementById("menu").style.left = "100vw";
+  } else {
+      document.getElementById("faqs").style.left = "0vw";
+      document.getElementById("about").style.left = "0vw";
+      document.getElementById("disclaimer").style.left = "50vw";
+  } 
+}
+
+function openDisclaimer() {
+  var b = window.matchMedia("(max-width: 900px)")  
+  if (b.matches) { // If less than 900px
+      document.getElementById("faqs").style.left = "0vw";
+      document.getElementById("about").style.left = "0vw";
+      document.getElementById("disclaimer").style.left = "0vw";
+      document.getElementById("menu").style.left = "100vw";
+  } else {
+      document.getElementById("faqs").style.left = "0vw";
+      document.getElementById("about").style.left = "0vw";
+      document.getElementById("disclaimer").style.left = "0vw";
+  }
+} 
+
 
 var acc = document.getElementsByClassName("accordion");
 var i;
@@ -6,10 +81,10 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.display = "block";
     }
   });
 }
